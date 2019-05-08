73f288aec90e6fd9b8a24755ac2566d1557a299d
V-Kunal Verma <V-Kunal.Verma@blackbaud.com>
2019-05-08 17:41:18 +0530

    a

--- precache-manifest.5742191c049454d713fccf5dca1eefe6.js
+++ precache-manifest.5742191c049454d713fccf5dca1eefe6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
     "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
-    "url": "/static/media/PokemonSolid.36d26dbc.ttf"
+    "url": "./static/media/PokemonSolid.36d26dbc.ttf"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "./static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "4fb73e60311a697c81a4",
-    "url": "/static/js/main.8372d6eb.chunk.js"
+    "url": "./static/js/main.8372d6eb.chunk.js"
   },
   {
     "revision": "934c5750b88644039e07",
-    "url": "/static/js/2.e1edc5f6.chunk.js"
+    "url": "./static/js/2.e1edc5f6.chunk.js"
   },
   {
     "revision": "4fb73e60311a697c81a4",
-    "url": "/static/css/main.b9c9b647.chunk.css"
+    "url": "./static/css/main.b9c9b647.chunk.css"
   },
   {
     "revision": "c3d2ef92f1d3af4e452b6d1812f62890",
